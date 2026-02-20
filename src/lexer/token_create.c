7ff7af1943f55898e2643310ae9368d4912b9248
grace haddad <gracehadda@gmail.com>
2026-02-21 01:12:01 +0200

    token operator type

---
+++ src/lexer/token_create.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_create.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grhaddad <grhaddad@student.42beirut.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/21 00:52:07 by grhaddad          #+#    #+#             */
+/*   Updated: 2026/02/21 00:52:07 by grhaddad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_token *ft_new_token(t_token_type type, char *value)
+{
+	t_token *token;
+
+	token = malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	
+}
--- src/lexer/token_type.c
+++ src/lexer/token_type.c
@@ -10,3 +10,43 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../includes/minishell.h"
+
+t_token *ft_get_double_operator_token(char *input, int *i)
+{
+	if (input[*i] == '<' && input[*i + 1] == '<')
+	{
+		*i += 2;
+		return (ft_new_token(TOKEN_HEREDOC, "<<"));
+	}
+	else if (input[*i] == '>' && input[*i + 1] == '>')
+	{
+		*i += 2;
+		return (ft_new_token(TOKEN_APPEND, ">>"));
+	}
+	return (NULL);
+}
+
+t_token	*ft_get_operator_token(char *input, int *i)
+{
+	if (input[*i] == '<' && input[*i + 1] == '<')
+		return (ft_get_double_operator_token(input, i));
+	if (input[*i] == '>' && input[*i + 1] == '>')
+		return (ft_get_double_operator_token(input, i));
+	else if (input[*i] == '<')
+	{
+		(*i)++;
+		return (ft_new_token(TOKEN_REDIR_IN, "<"));
+	}
+	else if (input[*i] == '>')
+	{
+		(*i)++;
+		return (ft_new_token(TOKEN_REDIR_OUT, ">"));
+	}
+	else if (input[*i] == '|')
+	{
+		(*i)++;
+		return (ft_new_token(TOKEN_PIPE, "|"));
+	}
+	return (NULL);
+}
