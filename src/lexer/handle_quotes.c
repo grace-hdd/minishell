ce61cf6cadf9578d300237b60dad516d0bed6a32
grace haddad <gracehadda@gmail.com>
2026-02-22 23:15:52 +0200

    add lexer: tokenize input into linked list of tokens

--- src/lexer/handle_quotes.c
+++ src/lexer/handle_quotes.c
@@ -10,3 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+t_token	*ft_get_quoted_token(char *input, int *i)
+{
+	int		start;
+	char	*word;
+	t_token	*tok;
+	char	quote;
+
+	quote = input[*i];
+	(*i)++;
+	start = *i;
+	while (input[*i] != '\0' && input[*i] != quote)
+		(*i)++;
+	if (input[*i] == '\0')
+		return (NULL);
+	word = ft_substr(input, start, *i - start);
+	(*i)++;
+	if (!word)
+		return (NULL);
+	tok = ft_new_token(TOKEN_WORD, word);
+	free(word);
+	return (tok);
+}
