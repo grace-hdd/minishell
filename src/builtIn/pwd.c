af4e9c818d46f683fcd62cbed74be64bb8fa0627
Grace Haddad <grhaddad@c2r3s3.42beirut.com>
2026-07-23 12:56:47 +0300

    implemented the pwd function

--- src/builtIn/pwd.c
+++ src/builtIn/pwd.c
@@ -6,7 +6,36 @@
 /*   By: grhaddad <grhaddad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 15:01:22 by grhaddad          #+#    #+#             */
-/*   Updated: 2026/07/23 10:19:18 by grhaddad         ###   ########.fr       */
+/*   Updated: 2026/07/23 12:45:39 by grhaddad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+int	pwd_cmd(t_shell *shell)
+{
+	char	*cwd;
+	char	*env_pwd;
+
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+		perror("getcwd failed");
+	if (cwd)
+	{
+		printf("%s\n", cwd);
+		free(cwd);
+		return (0);
+	}
+	env_pwd = ft_get_var_value("PWD", shell);
+	if (env_pwd && *env_pwd)
+	{
+		printf("%s\n", env_pwd);
+		free(env_pwd);
+		return (0);
+	}
+	if (env_pwd)
+		free(env_pwd);
+	ft_putendl_fd("minishell: pwd: error retrieving current directory",
+		STDERR_FILENO);
+	return (1);
+}
